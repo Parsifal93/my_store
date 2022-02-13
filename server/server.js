66537cb0466356e568a001fc629b6d40c1588fd5
Parsifal93 <vadimstratiienko@gmail.com>
2022-02-13 20:10:06 +0200

    add port 5000

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
 
 
+require('dotenv').config();
 
+const express  = require('express');
+
+const app =  express(); 
+
+const PORT  = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
