7dc69e4f31ce9d4d83ad68ecf7d6079b30cc682c
Parsifal93 <vadimstratiienko@gmail.com>
2022-02-14 21:26:55 +0200

    add Navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -2,24 +2,36 @@
    import {Link}  from 'react-router-dom';
    import "./index.css";
    
-   const Navbar
-    = () => {
+   const Navbar = () => {
      return (
        <nav className="navbar">
            <div className="navbar_logo">
              <h2>
-                   MERN Shopping Cart     
+                   Shopping Cart     
              </h2>
            </div>
            <ul className="navbar_links">
              <li>
                <Link to="/cart">
+                 <i className="fas fa-shoping-cart"></i>
+                 Cart
+                 <span className="cartlogo_babge">0</span>
+               </Link>
+             </li>
+             <li>
+               <Link to="/">
+                 Shop
                </Link>
              </li>
            </ul>
+           <div className="hamburger_menu">
+             <div></div>
+             <div></div>
+             <div></div>
+           </div>
 
        </nav>
      )
    }
    
-   export default Navbar
+   export default Navbar;
