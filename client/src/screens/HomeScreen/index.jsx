68bdea1d674b582ad4603d3ad4e0a75f051045b4
Parsifal93 <vadimstratiienko@gmail.com>
2022-02-15 00:17:32 +0200

    add product and product styles

--- client/src/screens/HomeScreen/index.jsx
+++ client/src/screens/HomeScreen/index.jsx
@@ -5,6 +5,10 @@ const HomeScreen = () => {
     return (
         <div className='homescreen'>
             <h2 className='homescreen-tilte'>Latest Product</h2>
+
+            <div className='homescreen_products'>
+                <Product />
+            </div>
         </div>
     );
 }
