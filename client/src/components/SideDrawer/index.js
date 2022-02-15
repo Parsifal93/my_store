35910b8753870c905850db5fcd26f377c8e2f6cd
Parsifal93 <vadimstratiienko@gmail.com>
2022-02-16 00:53:24 +0200

    add styles store

--- client/src/components/SideDrawer/index.js
+++ client/src/components/SideDrawer/index.js
@@ -1,11 +1,16 @@
 import "./index.css";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 
 const SideDrawer = ({ show, click }) => {
   const sideDrawerClass = ["sidedrawer"];
 
-  
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
 
   if (show) {
     sideDrawerClass.push("show");
@@ -19,7 +24,7 @@ const SideDrawer = ({ show, click }) => {
             <i className="fas fa-shopping-cart"></i>
             <span>
               Cart{" "}
-              <span className="sidedrawer__cartbadge"></span>
+              <span className="sidedrawer__cartbadge">{getCartCount()}</span>
             </span>
           </Link>
         </li>
