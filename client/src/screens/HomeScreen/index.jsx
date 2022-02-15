54805d8ecbf090e6c2ba301ef752aafb9da6c57f
Parsifal93 <vadimstratiienko@gmail.com>
2022-02-16 00:28:35 +0200

    add redux

--- client/src/screens/HomeScreen/index.jsx
+++ client/src/screens/HomeScreen/index.jsx
@@ -1,16 +1,42 @@
-
+import { useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {getProducts as listProducts} from '../../redux/actions/productActions';
+import Product from '../../components/Product';
 import './index.css';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+  
+    const getProducts = useSelector((state) => state.getProducts);
+    const { products, loading, error } = getProducts;
+  
+    useEffect(() => {
+      dispatch(listProducts());
+    }, [dispatch]);
+  
     return (
-        <div className='homescreen'>
-            <h2 className='homescreen-tilte'>Latest Product</h2>
-
-            <div className='homescreen_products'>
-                <Product />
-            </div>
+      <div className="homescreen">
+        <h2 className="homescreen__title">Latest Products</h2>
+        <div className="homescreen__products">
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : error ? (
+            <h2>{error}</h2>
+          ) : (
+            products.map((product) => (
+              <Product
+                key={product._id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                imageUrl={product.imageUrl}
+                productId={product._id}
+              />
+            ))
+          )}
         </div>
+      </div>
     );
-}
-
-export default HomeScreen;
+  };
+  
+  export default HomeScreen;
